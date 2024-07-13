2aa4f7c06c0b410550e13b2d7766a377cb0bb975
lugona <hpant7563@gmail.com>
2024-07-13 12:49:06 +0530

    creating some scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("./tasks/block-number");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 module.exports = {
+  defaultNetwork: "hardhat",
   solidity: "0.8.24",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  localhost: {
+    url: "http://127.0.0.1:8545/",
+    // accounts : already placed by hardhat default
+    chainId: 31337,
+  },
 };
