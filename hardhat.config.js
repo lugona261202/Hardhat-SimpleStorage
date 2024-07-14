8671c7267444f52bec2bf24029a97b2590ff0579
lugona <hpant7563@gmail.com>
2024-07-14 10:21:22 +0530

    adding module-export

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("./tasks/block-number");
+require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
